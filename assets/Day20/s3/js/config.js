c3c233b11103476add17d37969cbb37a83f8d5a3
Kei Kanazawa <kei.knzw@gmail.com>
2026-06-16 11:56:55 +0900

    draft: init

--- assets/Day20/s3/js/config.js
+++ assets/Day20/s3/js/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  API_BASE_URL: 'https://lpx4qj9rga.execute-api.ap-northeast-1.amazonaws.com', // API Gateway のデフォルトエンドポイントを入力
+  API_BASE_URL: '', // API Gateway のデフォルトエンドポイントを入力
 };
 
 window.CONFIG = CONFIG;
